d85af3cc230ca3ec09555b8c661e1f61ff31c580
Nathan Woodburn <nathan.woodburn2611@gmail.com>
2020-11-12 19:07:12 +1100

    Update

--- NJW Live Screen Project/Form1.Designer.cs
+++ NJW Live Screen Project/Form1.Designer.cs
@@ -32,7 +32,22 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.hideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.screenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.moveToScreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
@@ -52,6 +67,121 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hideToolStripMenuItem,
+            this.screenToolStripMenuItem,
+            this.moveToScreenToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(157, 92);
+            // 
+            // hideToolStripMenuItem
+            // 
+            this.hideToolStripMenuItem.Name = "hideToolStripMenuItem";
+            this.hideToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.hideToolStripMenuItem.Text = "Hide";
+            this.hideToolStripMenuItem.Click += new System.EventHandler(this.hideToolStripMenuItem_Click);
+            // 
+            // screenToolStripMenuItem
+            // 
+            this.screenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5});
+            this.screenToolStripMenuItem.Name = "screenToolStripMenuItem";
+            this.screenToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.screenToolStripMenuItem.Text = "View Screen";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(80, 22);
+            this.toolStripMenuItem2.Text = "1";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(80, 22);
+            this.toolStripMenuItem3.Text = "2";
+            this.toolStripMenuItem3.Visible = false;
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(80, 22);
+            this.toolStripMenuItem4.Text = "3";
+            this.toolStripMenuItem4.Visible = false;
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(80, 22);
+            this.toolStripMenuItem5.Text = "4";
+            this.toolStripMenuItem5.Visible = false;
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // moveToScreenToolStripMenuItem
+            // 
+            this.moveToScreenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7,
+            this.toolStripMenuItem8,
+            this.toolStripMenuItem9});
+            this.moveToScreenToolStripMenuItem.Name = "moveToScreenToolStripMenuItem";
+            this.moveToScreenToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.moveToScreenToolStripMenuItem.Text = "Move to Screen";
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(80, 22);
+            this.toolStripMenuItem6.Text = "1";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem6_Click);
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(80, 22);
+            this.toolStripMenuItem7.Text = "2";
+            this.toolStripMenuItem7.Visible = false;
+            this.toolStripMenuItem7.Click += new System.EventHandler(this.toolStripMenuItem7_Click);
+            // 
+            // toolStripMenuItem8
+            // 
+            this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+            this.toolStripMenuItem8.Size = new System.Drawing.Size(80, 22);
+            this.toolStripMenuItem8.Text = "3";
+            this.toolStripMenuItem8.Visible = false;
+            this.toolStripMenuItem8.Click += new System.EventHandler(this.toolStripMenuItem8_Click);
+            // 
+            // toolStripMenuItem9
+            // 
+            this.toolStripMenuItem9.Name = "toolStripMenuItem9";
+            this.toolStripMenuItem9.Size = new System.Drawing.Size(80, 22);
+            this.toolStripMenuItem9.Text = "4";
+            this.toolStripMenuItem9.Visible = false;
+            this.toolStripMenuItem9.Click += new System.EventHandler(this.toolStripMenuItem9_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "NJW Live Screen Project";
+            this.notifyIcon1.Visible = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -61,8 +191,10 @@
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Drag this form to the Stream";
+            this.Text = "NJW Live Screen Project";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -71,6 +203,20 @@
 
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem screenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moveToScreenToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem9;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- NJW Live Screen Project/Form1.cs
+++ NJW Live Screen Project/Form1.cs
@@ -18,6 +18,7 @@ namespace NJW_Live_Screen_Project
         }
         Rectangle rect;
         private Bitmap areaCapture;
+        int screen = 1;
         private void timer1_Tick(object sender, EventArgs e)
         {
             //timer1.Stop();
@@ -29,24 +30,24 @@ namespace NJW_Live_Screen_Project
             //        this.pictureBox1.Image = form.sentCapture; //this line returns an error that sentCapture does not exist
             //    }
             //}
-            int screen = 1;
-            foreach (string arg in Environment.GetCommandLineArgs())
-            {
-                if (arg.Length == 1)
-                {
-                    try
-                    {
-                        screen = int.Parse(arg);
-                        //MessageBox.Show(screen.ToString());
-                    }
-                    catch (Exception)
-                    {
+            
+            //foreach (string arg in Environment.GetCommandLineArgs())
+            //{
+            //    if (arg.Length == 1)
+            //    {
+            //        try
+            //        {
+            //            screen = int.Parse(arg);
+            //            //MessageBox.Show(screen.ToString());
+            //        }
+            //        catch (Exception)
+            //        {
 
-                    }
+            //        }
 
-                }
+            //    }
 
-            }
+            //}
             try
             {
 rect = new Rectangle(Screen.AllScreens[screen].WorkingArea.Location.X, Screen.AllScreens[screen].WorkingArea.Location.Y, Screen.AllScreens[screen].Bounds.Width, Screen.AllScreens[screen].Bounds.Height);
@@ -71,6 +72,125 @@ rect = new Rectangle(Screen.AllScreens[screen].WorkingArea.Location.X, Screen.Al
             this.FormBorderStyle = FormBorderStyle.None;
             this.ControlBox = false;
             this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void hideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (hideToolStripMenuItem.Text == "Hide")
+            {
+                hideToolStripMenuItem.Text = "Show";
+                this.Hide();
+            }
+            else
+            {
+                hideToolStripMenuItem.Text = "Hide";
+                this.Show();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int screens = Screen.AllScreens.Length;
+            switch (screens)
+            {
+                case 1:
+
+                    break;
+                case 2:
+                    toolStripMenuItem3.Visible = true;
+                    toolStripMenuItem7.Visible = true;
+
+
+                    break;
+                case 3:
+                    toolStripMenuItem3.Visible = true;
+                    toolStripMenuItem7.Visible = true;
+                    toolStripMenuItem4.Visible = true;
+                    toolStripMenuItem8.Visible = true;
+
+                    break;
+                case 4:
+                    toolStripMenuItem3.Visible = true;
+                    toolStripMenuItem7.Visible = true;
+                    toolStripMenuItem4.Visible = true;
+                    toolStripMenuItem8.Visible = true;
+                    toolStripMenuItem5.Visible = true;
+                    toolStripMenuItem9.Visible = true;
+              
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            screen = 0;
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            screen = 1;
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            screen = 2;
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            screen = 3;
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.Location = Screen.AllScreens[0].WorkingArea.Location;
+            this.Height = Screen.AllScreens[0].Bounds.Height;
+            this.Width = Screen.AllScreens[0].Bounds.Width;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ControlBox = false;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void toolStripMenuItem7_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.Location = Screen.AllScreens[1].WorkingArea.Location;
+            this.Height = Screen.AllScreens[1].Bounds.Height;
+            this.Width = Screen.AllScreens[1].Bounds.Width;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ControlBox = false;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void toolStripMenuItem8_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.Location = Screen.AllScreens[2].WorkingArea.Location;
+            this.Height = Screen.AllScreens[2].Bounds.Height;
+            this.Width = Screen.AllScreens[2].Bounds.Width;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ControlBox = false;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void toolStripMenuItem9_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.Location = Screen.AllScreens[3].WorkingArea.Location;
+            this.Height = Screen.AllScreens[3].Bounds.Height;
+            this.Width = Screen.AllScreens[3].Bounds.Width;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ControlBox = false;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
